e1c209dde3015a4c5d812acf92e2ecbd0294db92
kenkanekiken <kennethlim619@icloud.com>
2025-12-19 16:08:32 +0800

    Implemented Filter for listing page by listingType, ItemCategory, date posted.

---
+++ FoundBoxSG/ViewModels/ListingFilter.cs
@@ -0,0 +1,12 @@
+﻿namespace FoundBoxSG.ViewModels
+{
+    public enum ListingTypeFilter { All, Found, Lost }
+    public enum SortFilter { Latest, Oldest }
+
+    public class ListingFilter
+    {
+        public ListingTypeFilter Type { get; set; } = ListingTypeFilter.All;
+        public string? Category { get; set; } = null;  
+        public SortFilter Sort { get; set; } = SortFilter.Latest;
+    }
+}
