2acbf3631253a1a9e017cac9f2477a03ee2742bb
kenkanekiken <kennethlim619@icloud.com>
2025-12-21 19:17:17 +0800

    Implemented email notification to users email when recieved messages or replies.

---
+++ FoundBoxSG/Services/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace FoundBoxSG.Services;
+
+public class EmailService
+{
+    private readonly IConfiguration _config;
+
+    public EmailService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public async Task SendAsync(string toEmail, string subject, string body)
+    {
+        var smtp = new SmtpClient
+        {
+            Host = _config["Email:SmtpHost"],
+            Port = int.Parse(_config["Email:SmtpPort"]),
+            EnableSsl = true,
+            Credentials = new NetworkCredential(
+                _config["Email:Username"],
+                _config["Email:Password"]
+            )
+        };
+
+        var message = new MailMessage
+        {
+            From = new MailAddress(_config["Email:From"]),
+            Subject = subject,
+            Body = body,
+            IsBodyHtml = true
+        };
+
+        message.To.Add(toEmail);
+
+        await smtp.SendMailAsync(message);
+    }
+}
