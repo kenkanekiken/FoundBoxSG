cdcc34c7efb506700a302ef3d9aedc1e03bad1c3
kenkanekiken <kennethlim619@icloud.com>
2025-12-21 18:38:51 +0800

    Implemented notification bell and logic which shows notification when other user messages. Also edited the listing page in mobile view to be able to view entire listings.

--- FoundBoxSG/Domain/Messages.cs
+++ FoundBoxSG/Domain/Messages.cs
@@ -5,5 +5,6 @@
         public int MatchId { get; set; }
         public int SenderUserId { get; set; }
         public string? Content { get; set; }
+        public bool IsRead { get; set; } = false;
     }
 }
